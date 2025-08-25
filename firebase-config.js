e85e990a2ceeb330e1987ed2c9271c94e5b8cfb3
Satwik Saxena <satwiksaxena41@gmail.com>
2025-08-25 14:26:07 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcQ5oyLvJL5abrD95xl404_2UrBZjM2iE",
-  authDomain: "vehicleloginsystem.firebaseapp.com",
-  projectId: "vehicleloginsystem",
-  storageBucket: "vehicleloginsystem.appspot.com",
-  messagingSenderId: "335291119893",
-  appId: "1:335291119893:web:24bc54501717f10530d11c"
+  apiKey: "",
+  authDomain: "",
+  projectId: ""
+  storageBucket: "storage bucket",
+  messagingSenderId: "your sender id",
+  appId: your app id
 };
 
 const app = initializeApp(firebaseConfig);
